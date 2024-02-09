b83edacacc9c1de0a9917bb229265e2fae7184cc
Muhammad Ahtisham <shaffisultan30@gmail.com>
2024-02-09 15:14:45 +0400

    Added logger

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ app.use((req,res,next) => {
     next();
 });
 
-
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
 
 const MongoClient = require("mongodb").MongoClient;
 
